624f72cca81c0d6ee05b9a496fc27035fa70e5f2
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-04 23:40:13 +0300

    another commit

--- chapter_1/i_am_rich/lib/main.dart
+++ chapter_1/i_am_rich/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text("Iam rich", style: TextStyle(color: Colors.white)),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey[900],
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text("Iam rich", style: TextStyle(color: Colors.white),),
+            centerTitle: true,
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(image: AssetImage('assets/images/diamond.png')),
+          ),
         ),
-        body: Center(
-          child: Image(image: AssetImage('assets/images/diamond.png')),
-        ),
-      ),
-    ),
-  );
+      );
+  }
 }
