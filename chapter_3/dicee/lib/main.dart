5e05d51c4783996aa0f0dca7a3800b17ea54c019
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-08 19:40:55 +0300

    setup files

--- chapter_3/dicee/lib/main.dart
+++ chapter_3/dicee/lib/main.dart
@@ -14,6 +14,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dicee", style: TextStyle())),
+      ),
+    );
   }
 }
