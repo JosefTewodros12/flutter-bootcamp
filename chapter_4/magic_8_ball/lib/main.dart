ffda8310ef423d6d623cd5a8ff3837a7297fd4a0
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-09 12:55:36 +0300

    some changes

--- chapter_4/magic_8_ball/lib/main.dart
+++ chapter_4/magic_8_ball/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MagicBall());
@@ -12,13 +13,38 @@ class MagicBall extends StatefulWidget {
 }
 
 class _MagicBallState extends State<MagicBall> {
+  int imageNum = 1;
+  void changeImage() {
+    setState(() {
+      imageNum = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Ask me anything")),
-        body: Image.asset("assets/images/ball1.png"),
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text(
+            "Ask me anything",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 27,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              changeImage();
+            },
+            child: Image.asset("assets/images/ball$imageNum.png"),
+          ),
+        ),
       ),
     );
   }
