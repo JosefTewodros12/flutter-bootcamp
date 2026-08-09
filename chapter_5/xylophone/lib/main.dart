3675e1822366323a1a513fb562188d144a2e103c
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-09 21:32:15 +0300

    add some features

--- chapter_5/xylophone/lib/main.dart
+++ chapter_5/xylophone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,8 +11,42 @@ class XylophoneApp extends StatefulWidget {
 }
 
 class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(int sound) {
+    final player = AudioPlayer();
+    player.play(AssetSource("note$sound.wav"));
+  }
+
+  Expanded drawKey(int soundNum, Color color) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNum);
+        },
+        child: Container(color: color),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              drawKey(1, Colors.red),
+              drawKey(2, Colors.orange),
+              drawKey(3, Colors.yellow),
+              drawKey(4, Colors.green),
+              drawKey(5, Colors.teal),
+              drawKey(6, Colors.blue),
+              drawKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
