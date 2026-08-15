850a31782fa32ce6cf8f9dda952c5b66beedd074
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-15 22:38:28 +0300

    add reusable gensture detector

--- chapter_8/bmi_calculator/lib/components/reusable_card.dart
+++ chapter_8/bmi_calculator/lib/components/reusable_card.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({super.key, required this.colour, this.cardChild});
+  const ReusableCard({
+    super.key,
+    required this.colour,
+    this.cardChild,
+    this.onPress,
+  });
   final Color colour;
   final Widget? cardChild;
+  final VoidCallback? onPress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
