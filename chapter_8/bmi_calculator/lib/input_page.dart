30ec3effa3974538076c834f762da680162e6045
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-15 22:50:54 +0300

    config stm

--- chapter_8/bmi_calculator/lib/input_page.dart
+++ chapter_8/bmi_calculator/lib/input_page.dart
@@ -47,6 +47,11 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
                     colour: selectedGender == Gender.female
                         ? activeColour
                         : inactiveColour,
