40a7b92d1e69f1e0cc6bfca54c2972826f36518f
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-15 21:38:17 +0300

    add constant file

---
+++ chapter_8/bmi_calculator/lib/constants/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const btnColor = Color(0xFFEB1555);
+const activeColour = Color(0xFF1D1E33);
+const inactiveColour = Color(0xFF111328);
+
+Color maleColour = inactiveColour;
+Color femaleColour = inactiveColour;
