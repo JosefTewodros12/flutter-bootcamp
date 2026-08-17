5fe29f16a7dbac485ae99ef21c3358c1131cd349
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-17 12:31:59 +0300

    add round button components

--- chapter_8/bmi_calculator/lib/components/round_button.dart
+++ chapter_8/bmi_calculator/lib/components/round_button.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class RoundButton extends StatelessWidget {
-  const RoundButton({super.key});
-
+  const RoundButton({super.key, this.icon});
+  final FaIconData? icon;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return RawMaterialButton(
+      onPressed: () {},
+      elevation: 6,
+      shape: CircleBorder(),
+      fillColor: Color(0XFF4C4F5E),
+      constraints: const BoxConstraints.tightFor(width: 65.0, height: 65.0),
+      child: FaIcon(icon, size: 20),
+    );
   }
 }
