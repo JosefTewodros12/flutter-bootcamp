cd4802101e574d44397a2a023a5032cee27b659f
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-18 13:01:57 +0300

    change sth in reusable button

--- chapter_8/bmi_calculator/lib/components/reusable_button.dart
+++ chapter_8/bmi_calculator/lib/components/reusable_button.dart
@@ -3,20 +3,28 @@ import 'package:flutter/material.dart';
 class Reusablebutton extends StatelessWidget {
   final Color btnColour;
   final String btnText;
+  final VoidCallback onPress;
   const Reusablebutton({
     super.key,
     required this.btnColour,
     required this.btnText,
+    required this.onPress,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: btnColour,
-      height: 80,
-      child: Center(
-        child: Text(btnText, style: TextStyle(fontSize: 25, letterSpacing: 3)),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        width: double.infinity,
+        color: btnColour,
+        height: 80,
+        child: Center(
+          child: Text(
+            btnText,
+            style: TextStyle(fontSize: 25, letterSpacing: 3),
+          ),
+        ),
       ),
     );
   }
