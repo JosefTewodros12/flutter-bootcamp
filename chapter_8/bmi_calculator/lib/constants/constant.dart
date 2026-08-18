939a3e28d1e470bf564ab2e36122ffccfde6c834
Josef Tewodros <joseftewodros12@gmail.com>
2026-08-18 13:01:40 +0300

    add some constant text style

--- chapter_8/bmi_calculator/lib/constants/constant.dart
+++ chapter_8/bmi_calculator/lib/constants/constant.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+//INPUT PAGE COLORS AND STYLE
 const backgroundColour = Color(0xFF0A0E21);
 const activeColour = Color(0xFF1D1E33);
 const inactiveColour = Color(0xFF111328);
@@ -14,4 +15,24 @@ const numtextstyle = TextStyle(
   color: Colors.white,
   fontSize: 60,
   fontWeight: FontWeight.w900,
+);
+
+//RESULT PAGE COLORS AND STYLE
+const labelResultStyle = TextStyle(
+  color: Colors.green,
+  fontSize: 30,
+  fontWeight: FontWeight.w800,
+  textBaseline: TextBaseline.alphabetic,
+);
+
+const numResultStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 100,
+  fontWeight: FontWeight.bold,
+);
+
+const description = TextStyle(
+  color: Colors.white,
+  fontSize: 25,
+  textBaseline: TextBaseline.alphabetic,
 );
